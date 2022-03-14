ec86c3feefb1617606beeac9a8f65eb712635488
Guillaume Dumoulin <guillaume.dumoulin@hotmail.fr>
2022-03-14 21:30:42 +0100

    fake sonar with button that trigger detection + timer for timeout sound

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h>
 #include <HardwareSerial.h>
-#include <HCSR04.h>
 
-#include <Button.h>
-#include <blinky.h>
 
 #include <DFRobotDFPlayerMini.h>
 #include <OneButton.h>
 #include <Player.h>
 #include <HumanDetection.h>
+#include <arduino-timer.h>
 
 #define BUTTON_PIN 27
+// int delay_to_stop = 1000 * 1 * 60 * 5;
+int delay_to_stop = 1000 * 5;
 
 OneButton btn = OneButton(
   BUTTON_PIN,  // Input pin for the button
@@ -18,51 +18,58 @@ OneButton btn = OneButton(
   false         // Enable internal pull-up resistor
 );
 
+auto timer = timer_create_default();
 Player player = Player();
 HumanDetection detector = HumanDetection();
 
 // Handler function for a single click:
 static void handleClick() {
   Serial.println("Clicked!");
+
+  detector.trigger();
+}
+
+bool stopSound(void *argument /* optional argument given to in/at/every */) {
+  player.fadeOut();
+  Serial.println("<--- stop sound");
+  return true;
+}
+
+void startSound() {
   player.fadeIn();
+  Serial.println("---> start sound");
+  timer.cancel();
+  timer.in(delay_to_stop, stopSound );
 }
 
 void setup() {
   Serial.begin(115200);
 
-  // blinky_init();
-  // Button btn = Button(1);
-  // btn.debug();
 
-  // Single Click event attachment
   btn.attachClick(handleClick);
-
-  // // Double Click event attachment with lambda
   btn.attachDoubleClick([]() {
     // Serial.println("Double Pressed!");
-    player.fadeOut();
+    // player.fadeOut();
   });
-
-  // btn.attachMultiClick([]() {
-  //   Serial.println("MultiClick Pressed!");
-  // });
 
   btn.attachLongPressStart([]() {
     // Serial.println("Long press start");
     player.stopFade();
   });
 
-  // btn.attachDuringLongPress([]() {
-  //   Serial.println("Long press progress");
-  // });
-
-  // btn.attachLongPressStop([]() {
-  //   Serial.println("Long press stop");
-  // });
+  detector.attachDetection([]() {
+    Serial.println("Human detected !");
+    startSound();
+  });
 }
 
+
+
+
+
 void loop() {
-  // do_blink(500);
+  timer.tick();
   btn.tick();
   player.tick();
+  detector.tick();
 }
