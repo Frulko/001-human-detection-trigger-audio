363a4eed4c907a811c107236abd5a805f1dce898
Guillaume Dumoulin <guillaume.dumoulin@hotmail.fr>
2022-03-14 21:30:15 +0100

    feat: add full class + callback

--- src/HumanDetection.cpp
+++ src/HumanDetection.cpp
@@ -1,7 +1,41 @@
 #include <HumanDetection.h>
 #include <Arduino.h>
 #include <NewPing.h>
+#include <vector>
 
-HumanDetection::HumanDetection(){
 
+HumanDetection::HumanDetection()
+{
+  std::vector<NewPing> sonars = {
+    NewPing(19, 18, maxDistance),
+    NewPing(19, 21, maxDistance)
+  };
+}
+
+void HumanDetection::trigger()
+{
+  if(_humanDetectionFunc) _humanDetectionFunc();
+  if(_paramHumanDetectionFunc) _paramHumanDetectionFunc(_humanDetectionFuncParam);
+}
+
+void HumanDetection::attachDetection(callbackFunction newFunction)
+{
+  _humanDetectionFunc = newFunction;
+}
+
+void HumanDetection::attachDetection(parameterizedCallbackFunction newFunction, void *parameter)
+{
+  _paramHumanDetectionFunc = newFunction;
+  _humanDetectionFuncParam = parameter;
+}
+
+void HumanDetection::tick()
+{
+  for (int i = 0; i < sonars.size(); ++i) {
+    delay(30); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+    Serial.print(i);
+    Serial.print("=");
+    Serial.print(sonars[i].ping_cm());
+    Serial.print("cm ");
+  }
 }
--- src/HumanDetection.h
+++ src/HumanDetection.h
@@ -1,12 +1,31 @@
 #ifndef HUMANDETECTION_H /* include guards */
 #define HUMANDETECTION_H
+#include <NewPing.h>
+#include <vector>
+
+extern "C" {
+typedef void (*callbackFunction)(void);
+typedef void (*parameterizedCallbackFunction)(void *);
+}
 
 class HumanDetection
 {
   private: 
-    
+    char name[30];
+    std::vector<NewPing> sonars;
+    int numberOfSonar = 2; // Number of sensors.
+    int maxDistance = 200; // Maximum distance (in cm) to ping.
+
+    callbackFunction _humanDetectionFunc = NULL;
+    parameterizedCallbackFunction _paramHumanDetectionFunc = NULL;
+    void *_humanDetectionFuncParam = NULL;
+
   public: 
     HumanDetection();
+    void tick();
+    void trigger();
+    void attachDetection(callbackFunction newFunction);
+    void attachDetection(parameterizedCallbackFunction newFunction, void *parameter);
     
 };
 
