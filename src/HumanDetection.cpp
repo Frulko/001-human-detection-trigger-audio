e4b181535fb7a033e69080752fa8329f509b8871
Guillaume Dumoulin <guillaume.dumoulin@hotmail.fr>
2022-03-16 14:05:56 +0100

    feat: add hc-sr04 detection for multiple sonar

--- src/HumanDetection.cpp
+++ src/HumanDetection.cpp
@@ -4,12 +4,23 @@
 #include <vector>
 
 
+
 HumanDetection::HumanDetection()
 {
-  std::vector<NewPing> sonars = {
+  
+}
+
+void HumanDetection::init()
+{
+  Serial.println("Human starter");
+  sonars = {
     NewPing(19, 18, maxDistance),
     NewPing(19, 21, maxDistance)
   };
+
+
+  Serial.print("size");
+  Serial.println(sonars.size());
 }
 
 void HumanDetection::trigger()
@@ -32,10 +43,40 @@ void HumanDetection::attachDetection(parameterizedCallbackFunction newFunction, 
 void HumanDetection::tick()
 {
   for (int i = 0; i < sonars.size(); ++i) {
-    delay(30); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-    Serial.print(i);
-    Serial.print("=");
-    Serial.print(sonars[i].ping_cm());
-    Serial.print("cm ");
+    delay(50);  // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+
+    int distance = sonars.at(i).ping_cm();
+
+    if (distance < 1) { // returns true once every 10 ms, determines sampling frequency
+      return;
+    }
+
+    bool canUpdate = compareDistance(distance, previousDistance[i], i);
+
+    if(canUpdate) {
+      previousDistance.at(i) = distance;
+    }
   }
 }
+
+bool HumanDetection::compareDistance(int distance, int previousDistance, int index)
+{
+   if (distance < 1) {
+   return false;
+  }
+
+  int diff = abs(distance - previousDistance);
+
+  if(diff > deltaDetection){
+    Serial.print("trigger cut: ");
+    Serial.print(diff);
+    Serial.print(" - ");
+    Serial.print(distance);
+    Serial.print(",");
+    Serial.println(previousDistance);
+    trigger();
+  }
+
+  return true;
+}
+
--- src/HumanDetection.h
+++ src/HumanDetection.h
@@ -2,6 +2,10 @@
 #define HUMANDETECTION_H
 #include <NewPing.h>
 #include <vector>
+// #include <Filters.h>
+
+// #include <AH/STL/cmath>                    // std::round
+// #include <Filters/MedianFilter.hpp>        // MedianFilter
 
 extern "C" {
 typedef void (*callbackFunction)(void);
@@ -13,20 +17,24 @@ class HumanDetection
   private: 
     char name[30];
     std::vector<NewPing> sonars;
-    int numberOfSonar = 2; // Number of sensors.
-    int maxDistance = 200; // Maximum distance (in cm) to ping.
 
+    // std::vector<MedianFilter<10, uint16_t>> mediaFilters;
+    std::vector<int> previousDistance = {0, 0};
+    int numberOfSonar = 2; // Number of sensors.
+    int maxDistance = 500; // Maximum distance (in cm) to ping.
+    int deltaDetection = 70;
     callbackFunction _humanDetectionFunc = NULL;
     parameterizedCallbackFunction _paramHumanDetectionFunc = NULL;
     void *_humanDetectionFuncParam = NULL;
+    bool compareDistance(int distance, int previousDistance, int index);
 
   public: 
     HumanDetection();
+    void init();
     void tick();
     void trigger();
     void attachDetection(callbackFunction newFunction);
     void attachDetection(parameterizedCallbackFunction newFunction, void *parameter);
-    
 };
 
 #endif /* HUMANDETECTION_H */
